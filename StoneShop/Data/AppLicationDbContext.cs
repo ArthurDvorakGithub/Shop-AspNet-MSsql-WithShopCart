b07a72f9a60f5274393cbc540397c1924e26b498
Arthur Dvorak <artdvorak@yandex.ru>
2022-05-25 09:54:15 +0300

    Shop of stone

---
+++ StoneShop/Data/AppLicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StoneShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneShop.Data
+{
+    public class AppLicationDbContext : IdentityDbContext
+    {
+        public AppLicationDbContext(DbContextOptions<AppLicationDbContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
---
+++ StoneShop/WC.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoneShop
+{
+    public class WC
+    {
+        public static string ImagePath = @"\images\product\";
+        public const string SessionCart = "ShoppingCartSession";
+
+        public const string AdminRole = "Admin";
+        public const string CustomerRole = "Customer";
+
+        public const string EmailAdmin = "artdvorak@yandex.ru";
+    }
+}
